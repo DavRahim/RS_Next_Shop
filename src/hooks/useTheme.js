2f941b8c2c0a2e8ee3615a384d323debabc27bde
Md Abdur Rahim <117851075+DavRahim@users.noreply.github.com>
2024-02-05 21:03:01 +0600

    error handle on project

--- src/hooks/useTheme.js
+++ src/hooks/useTheme.js
@@ -3,6 +3,7 @@ import { useContext } from "react";
 
 const useTheme = () => {
   const theme = useContext(ThemeContext);
+
   const isClient = typeof window !== "undefined";
 
   if (!isClient && !theme) return {};
